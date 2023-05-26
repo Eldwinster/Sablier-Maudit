36b7540786b0081679028958706bca4466f75705
Eldwinster <yann.arby@gmail.com>
2023-05-26 14:09:19 +0200

    added bare main window

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
-import datetime
 import tkinter as tk
 from tkinter import ttk
+
+# main window
+root = tk.Tk()
+root.title('Sablier Maudit')
+root.geometry('800x600')
+
+root.mainloop()
