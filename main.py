da7242876a83f45cb855e874a266f91b73d69df0
Eldwinster <yann.arby@gmail.com>
2023-06-25 17:46:02 +0200

    test

--- main.py
+++ main.py
@@ -21,10 +21,25 @@ Plus le sablier se retourne moins vous avez de temps.
 Réussirez-vous à battre le sablier ou allez vous succombez sous le poids de sa malédiction ?
 """
 
+hourVar = ""
+minVar = ""
+secVar = ""
+incrementVar = ""
+loopVar = ""
+
 
 def padding(frame):
     for child in frame.winfo_children():
         child.grid_configure(padx=10, pady=10)
+
+
+def countdown():
+    try:
+        # get the initial time and convert it in seconds
+        tmp_time = hourVar.get() * 3600 + minVar.get() * 60 + secVar.get()
+    except:
+        print("error")
+
 
 
 class tkinterApp(tk.Tk):
@@ -37,7 +52,7 @@ class tkinterApp(tk.Tk):
 
         # creating a container
         container = tk.Frame(self)
-        container.pack(side="top", fill="both", expand=True)
+        container.pack(anchor=tk.CENTER, side="top", fill="both", expand=True)
 
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
@@ -95,7 +110,6 @@ class WelcomePage(tk.Frame):
 class SettingsPage(tk.Frame):
 
     def __init__(self, parent, controller):
-
         tk.Frame.__init__(self, parent)
 
         # Labels
@@ -103,35 +117,61 @@ class SettingsPage(tk.Frame):
         incrementLabel = ttk.Label(self, text="Incrément/Décrément : ")
         loopLabel = ttk.Label(self, text="Nombre de tours : ")
 
+        hourLabel = ttk.Label(self, text="h")
+        minLabel = ttk.Label(self, text="m")
+        secLabel = ttk.Label(self, text="s")
+
         # Settings
-        hourVar = tk.IntVar()
-        minVar = tk.IntVar()
-        secVar = tk.IntVar()
-        incrementVar = tk.IntVar()
-        loopVar = tk.IntVar()
+        global hourVar, minVar, secVar, incrementVar, loopVar
+        hourVar = tk.StringVar()
+        minVar = tk.StringVar()
+        secVar = tk.StringVar()
+        incrementVar = tk.StringVar()
+        loopVar = tk.StringVar()
+        inc = tk.StringVar()
+        dec = tk.StringVar()
 
         # Entries
-        hourEntry = ttk.Entry(self, textvariable=hourVar, width=3)
-        minEntry = ttk.Entry(self, textvariable=minVar, width=3)
-        secEntry = ttk.Entry(self, textvariable=secVar, width=3)
+        hourEntry = ttk.Entry(self, textvariable=hourVar, width=2)
+        minEntry = ttk.Entry(self, textvariable=minVar, width=2)
+        secEntry = ttk.Entry(self, textvariable=secVar, width=2)
         incrementEntry = ttk.Entry(self, textvariable=incrementVar, width=3)
         loopEntry = ttk.Entry(self, textvariable=loopVar, width=3)
+
+        # Checkbox
+        incrementCheckbox = ttk.Checkbutton(self, text="Incrément", variable=inc)
+        decrementCheckbox = ttk.Checkbutton(self, text="Décrément", variable=dec)
+
+        # button
+        quit_button = ttk.Button(self, text="Quitter",
+                                 command=lambda: self.quit())
+        apply_button = ttk.Button(self, text="Lancer",
+                                  command=lambda: controller.show_frame(CountdownPage))
 
         # Label placement
         timeLabel.grid(row=0, column=1)
         incrementLabel.grid(row=1, column=1)
         loopLabel.grid(row=2, column=1)
 
-        # Entries placement
-        hourEntry.grid(row=0, column=2)
-        minEntry.grid(row=0, column=3)
-        secEntry.grid(row=0, column=4)
-        incrementEntry.grid(row=1, column=2)
-        loopEntry.grid(row=2, column=2)
+        hourLabel.grid(row=0, column=11)
+        minLabel.grid(row=0, column=21)
+        secLabel.grid(row=0, column=31)
 
-        quit_button = ttk.Button(self, text="Quitter",
-                                 command=lambda: self.quit())
+        # Entries placement
+        hourEntry.grid(row=0, column=10)
+        minEntry.grid(row=0, column=20)
+        secEntry.grid(row=0, column=30)
+        incrementEntry.grid(row=1, column=10)
+        loopEntry.grid(row=2, column=10)
+
+        # Checkbox placement
+        incrementCheckbox.grid(row=1, column=20)
+        decrementCheckbox.grid(row=1, column=30)
+
+        # Button placement
         quit_button.grid(row=10, column=0)
+        apply_button.grid(row=10, column=40)
+
         padding(self)
 
 
@@ -139,26 +179,45 @@ class SettingsPage(tk.Frame):
 class CountdownPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        label = ttk.Label(self, text ="Page 2")
-        label.grid(row = 0, column = 4)
+
+        hourLabel = ttk.Label(
+            self,
+            textvariable=hourVar,
+            width=2,
+            font=("Roboto", 100, "")
+        )
+
+        minLabel = ttk.Label(
+            self,
+            textvariable=minVar,
+            width=2,
+            font=("Roboto", 100, "")
+        )
+
+        secLabel = ttk.Label(
+            self,
+            textvariable=secVar,
+            width=2,
+            font=("Roboto", 100, "")
+        )
+
+        hourLabel.grid(row=2, column=10)
+        minLabel.grid(row=2, column=20)
+        secLabel.grid(row=2, column=30)
 
         # button to show frame 2 with text
         # layout2
-        button1 = ttk.Button(self, text ="Page 1",
-                            command = lambda : controller.show_frame(SettingsPage))
+        quit_button = ttk.Button(self, text="Quitter",
+                                 command=lambda: self.quit())
+
+        quit_button.grid(row=10, column=0)
+
+        prev_button = ttk.Button(self, text ="Précédent",
+                                 command=lambda: controller.show_frame(SettingsPage))
 
         # putting the button in its place by
         # using grid
-        button1.grid(row = 1, column = 1)
-
-        # button to show frame 3 with text
-        # layout3
-        button2 = ttk.Button(self, text ="Startpage",
-                            command = lambda : controller.show_frame(WelcomePage))
-
-        # putting the button in its place by
-        # using grid
-        button2.grid(row = 2, column = 1)
+        prev_button.grid(row = 10, column = 100)
 
         padding(self)
 
