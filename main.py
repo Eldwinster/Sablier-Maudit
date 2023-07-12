a06200ea9c867c9c388e66a8a7c365475954765a
Eldwinster <yann.arby@gmail.com>
2023-07-13 00:24:32 +0200

    added a new frame
    I added a new frame where I count the turn and some other small features

--- main.py
+++ main.py
@@ -20,33 +20,118 @@ BACKGROUND = 'black'
 FOREGROUND = 'white'
 
 def countdown():
-    paramFrame = ttk.Frame(root, padding='3 3 12 12')
-    paramFrame.grid(row=0, column=0)
-    paramFrame.columnconfigure(0, weight=1)
-    paramFrame.rowconfigure(0, weight=1)
+    global loop
+    turnFrame = ttk.Frame(root, padding=PADDING)
+    turnFrame.grid(row=0, column=0)
+    turnFrame.columnconfigure(0, weight=1)
+    turnFrame.rowconfigure(0, weight=1)
 
-    hourLabel = ttk.Label(paramFrame, textvariable=hourVar, font=FONT)
-    minLabel = ttk.Label(paramFrame, textvariable=minVar, font=FONT)
-    secLabel = ttk.Label(paramFrame, textvariable=secVar, font=FONT)
-    sep1Label = ttk.Label(paramFrame, text=":", font=FONT)
-    sep2Label = ttk.Label(paramFrame, text=":", font=FONT)
+    turnLabel1 = ttk.Label(
+        turnFrame,
+        text='Tour: ',
+        font=TXT_FONT,
+        background=BACKGROUND,
+        foreground=FOREGROUND
+    )
 
-    hourLabel.grid(row=0, column=0)
-    sep1Label.grid(row=0, column=1)
-    minLabel.grid(row=0, column=2)
-    sep2Label.grid(row=0, column=3)
-    secLabel.grid(row=0, column=4)
+    turnLabel2 = ttk.Label(
+        turnFrame,
+        textvariable=turnVar,
+        font=TXT_FONT,
+        background=BACKGROUND,
+        foreground=FOREGROUND
+    )
+
+    turnLabel3 = ttk.Label(
+        turnFrame,
+        text='/9',
+        font=TXT_FONT,
+        background=BACKGROUND,
+        foreground=FOREGROUND
+    )
+
+    turnLabel1.grid(row=0, column=0)
+    turnLabel2.grid(row=0, column=1)
+    turnLabel3.grid(row=0, column=2)
+
+    countdownFrame = ttk.Frame(root, padding=PADDING)
+    countdownFrame.grid(row=1, column=0)
+    countdownFrame.columnconfigure(0, weight=1)
+    countdownFrame.rowconfigure(0, weight=1)
+
+
+    hourLabel = ttk.Label(
+        countdownFrame,
+        textvariable=hourVar,
+        font=NUM_FONT,
+        background=BACKGROUND,
+        foreground=FOREGROUND
+    )
+    sep1Label = ttk.Label(
+        countdownFrame,
+        text=":",
+        font=NUM_FONT,
+        background=BACKGROUND,
+        foreground=FOREGROUND
+    )
+    minLabel = ttk.Label(
+        countdownFrame,
+        textvariable=minVar,
+        font=NUM_FONT,
+        background=BACKGROUND,
+        foreground=FOREGROUND
+    )
+    sep2Label = ttk.Label(
+        countdownFrame,
+        text=":",
+        font=NUM_FONT,
+        background=BACKGROUND,
+        foreground=FOREGROUND
+    )
+    secLabel = ttk.Label(
+        countdownFrame,
+        textvariable=secVar,
+        font=NUM_FONT,
+        background=BACKGROUND,
+        foreground=FOREGROUND
+    )
+
+    hourLabel.grid(row=1, column=0)
+    sep1Label.grid(row=1, column=1)
+    minLabel.grid(row=1, column=2)
+    sep2Label.grid(row=1, column=3)
+    secLabel.grid(row=1, column=4)
 
 
     try:
         tmp = int(hourVar.get()) * HOUR + int(minVar.get()) * MINUTE + int(secVar.get())
         initial_timer = tmp
+        tempsVariable = int(hourVar.get()) * HOUR + int(minVar.get()) * MINUTE + int(secVar.get())
+
+        timeMem = tempsVariable
+
+        timeToDecrement = tempsVariable
+
     except:
         print("Please input the right value")
 
-    while initial_timer > 0:
-        mins, secs = divmod(tmp, MINUTE)
-        hours = 00
+    while loop >= 0:
+        mins, secs = divmod(tempsVariable, MINUTE)
+        hours = 0
+
+        if loop == 1:
+            turnLabel1.destroy()
+            turnLabel2.destroy()
+            turnLabel3.destroy()
+
+            lastTurnLabel = ttk.Label(
+                turnFrame,
+                text='Dernier Tour !',
+                font=NUM_FONT,
+                background=BACKGROUND,
+                foreground='red'
+            )
+            lastTurnLabel.grid(row=0, column=0)
 
         if mins > MINUTE:
             hours, mins = divmod(mins, MINUTE)
@@ -54,27 +139,33 @@ def countdown():
         hourVar.set("{0:2d}".format(hours))
         minVar.set("{0:2d}".format(mins))
         secVar.set("{0:2d}".format(secs))
+        turnVar.set("{0:2d}".format(loop))
 
         # tentative de formatage des variables en text sans succes
         # time_string = f'{hourVar:2d}:{minVar:2d}:{hourVar:2d}'
         # label.config(text=time_string)
-        #
+
         root.update()
         time.sleep(1)
 
-        if (tmp == 10):
+        if (tempsVariable == 10):
             hourLabel.config(foreground="red")
             sep1Label.config(foreground="red")
             minLabel.config(foreground="red")
             sep2Label.config(foreground="red")
             secLabel.config(foreground="red")
 
-        if (tmp == 0):
-            initial_timer -= DECREMENT
-            tmp = initial_timer
-        tmp -= 1
+        if (tempsVariable == 0):
+            timeToDecrement -= DECREMENT
+            # impossible de à mettre freeze le timer
+            loop -= 1
+            tempsVariable = timeToDecrement
+        if (tempsVariable < 0):
+            tempsVariable = timeMem
 
-        if (initial_timer == 0):
+        tempsVariable -= 1
+
+        if (loop == 0):
             tk.messagebox.showinfo("Timer: ", "Time's up")
 
     # while tmp_time > 0:
@@ -85,19 +176,24 @@ def countdown():
 root = tk.Tk()
 root.title('Sablier Maudit')
 root.geometry("800x600")
-root.after(100, countdown)
-root.resizable(True, True)
 root.attributes("-fullscreen", True)
+root.resizable(True, True)
+root.configure(background=BACKGROUND)
+
+root.after(100, countdown)
+
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 
 hourVar = tk.StringVar()
 minVar = tk.StringVar()
 secVar = tk.StringVar()
+turnVar = tk.StringVar()
 
-hourVar.set('00')
-minVar.set('00')
-secVar.set('20')
+hourVar.set(hours)
+minVar.set(minutes)
+secVar.set(seconds)
+turnVar.set(loop)
 
 # tentative de conversion en string pour le label
 # t = "%2d:%2d:2%d"(hourVar,minVar,secVar)
